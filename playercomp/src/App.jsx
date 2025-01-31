db7fa5b5563c61a039bf37854ab4f4d8ad706e5a
roadgoat19 <123213767+roadgoat19@users.noreply.github.com>
2025-01-31 13:21:22 -0500

    added vercel analytics

--- playercomp/src/App.jsx
+++ playercomp/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import PlayerComparisonPage from "./pages/PlayerComparisonPage"; // Your main page
 import About from "./pages/About"; // Your about page
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
+      <Analytics />
     </Router>
   );
 };
